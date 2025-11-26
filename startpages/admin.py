5ab95a44654c0c1a9ca996254dde70d9fc3fa3b0
Mathi <Mathias.meersschaut@icloud.com>
2025-11-26 19:00:11 +0100

    models

--- startpages/admin.py
+++ startpages/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import StartPage, Section, Link
 
-# Register your models here.
+class LinkInline(admin.TabularInline):
+    model = Link
+    extra = 1
+    fields = ('name', 'url', 'order')
+    sortable_field_name = "order"
+
+class SectionAdmin(admin.ModelAdmin):
+    inlines = [LinkInline]
+    list_display = ('name', 'page', 'order')
+    list_editable = ('order',)
+
+class SectionInline(admin.TabularInline):
+    model = Section
+    extra = 0
+    show_change_link = True
+    fields = ('name', 'order')
+
+class StartPageAdmin(admin.ModelAdmin):
+    inlines = [SectionInline]
+    list_display = ('user', 'title', 'slug')
+    list_filter = ('user',)
+    search_fields = ('user__username', 'title')
+
+admin.site.register(StartPage, StartPageAdmin)
+admin.site.register(Section, SectionAdmin)
+admin.site.register(Link)
--- startpages/models.py
+++ startpages/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
-# Create your models here.
+class StartPage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="startpages")
+    title = models.CharField(max_length=100, default="My Startpage")
+    slug = models.SlugField(unique=True, editable=False, max_length=255)
+
+    class Meta:
+        unique_together = ('user', 'title')
+        verbose_name = "Start Page"
+        verbose_name_plural = "Start Pages"
+        
+    def save(self, *args, **kwargs):
+        if self.user and self.title:
+            base_slug = f"{self.user.username}-{self.title}"
+            self.slug = slugify(base_slug)
+        super().save(*args, **kwargs)
+          
+    def __str__(self):
+        return self.title
+
+class Section(models.Model):
+    page = models.ForeignKey(StartPage, related_name='sections', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    order = models.PositiveIntegerField(default=0, help_text="Lower numbers appear first")
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"{self.name} ({self.page.title})"
+
+class Link(models.Model):
+    section = models.ForeignKey(Section, related_name='links', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    url = models.URLField(max_length=500)    
+    order = models.PositiveIntegerField(default=0, help_text="Lower numbers appear first")
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
