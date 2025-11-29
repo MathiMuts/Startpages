706f179b8920a4cebfed8064880b02baa589a338
Mathi <Mathias.meersschaut@icloud.com>
2025-11-29 18:15:49 +0100

    colours

--- startpages/admin.py
+++ startpages/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GlobalSettings, StartPage, Section, Link
+from .models import GlobalSettings, StartPage, Section, Link, ColorScheme
 
 class LinkInline(admin.TabularInline):
     model = Link
@@ -24,7 +24,22 @@ class StartPageAdmin(admin.ModelAdmin):
     list_filter = ('user',)
     search_fields = ('user__username', 'title')
 
-admin.site.register(GlobalSettings)
+class ColorSchemeInline(admin.StackedInline):
+    model = ColorScheme
+    extra = 0
+    fields = ('name', 'is_dark', 'preview_colors', 'css_variables')
+    help_text = "Define Global Themes here."
+
+class GlobalSettingsAdmin(admin.ModelAdmin):
+    inlines = [ColorSchemeInline]
+    
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        return super().has_add_permission(request)
+
+admin.site.register(GlobalSettings, GlobalSettingsAdmin)
 admin.site.register(StartPage, StartPageAdmin)
 admin.site.register(Section, SectionAdmin)
 admin.site.register(Link)
+admin.site.register(ColorScheme)
